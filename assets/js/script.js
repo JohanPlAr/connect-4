0b97c8b23d741b2d994d0baf10eac7f911829ef5
Johan Plym Arkert <johan.arkert@gmail.com>
2023-08-18 15:57:44 +0200

    Add eventlisteners to main-menu buttons

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+// display the game and set main menu to display none when click on open game btn
+// open rules modal when click on rules button
+
+document.addEventListener("DOMContentLoaded", function () {
+  const openGameBtn = document.getElementById("open-game-btn");
+  const readRulesBtn = document.getElementById("read-rules-btn");
+  const theGame = document.querySelector(".the-game");
+  const mainMenu = document.querySelector(".main-menu");
+  
+  openGameBtn.addEventListener("click", () => {
+    theGame.style.display = "block";
+    mainMenu.style.display = "none";
+
+    readRulesBtn.addEventListener("click", () => {
+        rulesModal.style.display = "flex";
+        theGame.style.display = "none";
+      });
+  });
+});
+
+function runGame() {}
