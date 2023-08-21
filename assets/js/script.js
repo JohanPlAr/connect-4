fdf8464a1b3f8352f5d9ea92d6828f71d1aa7234
Johan Plym Arkert <johan.arkert@gmail.com>
2023-08-21 08:23:24 +0200

    Add choseColumn function and comments to markBox

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
-// display the game and set main menu to display none when click on open game btn
-// open rules modal when click on rules button
-// close read rules modal when clicking on span
+// Display the game and set main menu to display none when click on open game btn
+// Open rules modal when click on rules button
+// Close read rules modal when clicking on span
 
 document.addEventListener("DOMContentLoaded", function () {
   const openGameBtn = document.getElementById("open-game-btn");
@@ -40,11 +40,19 @@ function runGame() {
     document.getElementById("p1-turn").style.backgroundColor = "red";
     document.getElementById("p1-turn-text").innerText = "Next Play";
 
-    function markColumn() {
+
+    //Function is used to show player where game coin will end up
+    //Add event listeners to game column
+    //Create array of boxes from marked column
+    //Filter boxes have for class "active". 
+    //If class is not active box background color 
+    //Remove background color on mouse out 
+        
+    function markBox() {
         let markedColumns = document.querySelectorAll(".game-column");
         markedColumns.forEach(function (column) {
           column.addEventListener("mouseover", function () {
-            column.style.backgroundColor = "";
+            // column.style.backgroundColor = "cyan"; can be used to display marked column 
             let boxes = Array.from(column.querySelectorAll(".game-box"));
             boxes = boxes.filter((x) => !x.classList.contains("active"));
             console.log(boxes);
@@ -66,5 +74,38 @@ function runGame() {
           });
         });
       }
-      markColumn()
+      markBox()
+
+      function choseColumn() {
+        let playerColumn = 0;
+        for (let i = 0; i < gameColumns.length; i++)
+          gameColumns[i].addEventListener("click", function () {
+            playerColumn = i;
+            console.log(playerColumn);
+            console.log(turn);
+            if (turn % 2 === 0) {
+              addCoin(playerColumn, "red");
+              document.getElementById("p1-turn").style.backgroundColor = "white";
+              document.getElementById("p2-turn").style.backgroundColor = "yellow";
+              document.getElementById("p2-turn-text").innerText = "Next Play";
+              document.getElementById("p1-turn-text").innerText = "";
+            } else {
+              addCoin(playerColumn, "yellow");
+              document.getElementById("p2-turn").style.backgroundColor = "white";
+              document.getElementById("p1-turn").style.backgroundColor = "red";
+              document.getElementById("p1-turn-text").innerText = "Next Play";
+              document.getElementById("p2-turn-text").innerText = "";
+            }
+          });
+          function addCoin(playerColumn, color) {
+            turn += 1;
+            if (gameArrays[playerColumn].length < 6) {
+              gameArrays[playerColumn].push(color);
+            }
+            console.log(gameArrays[playerColumn]);
+            displayCoins(color);
+            checkWinner(color);
+          }
+      }
+
 }
